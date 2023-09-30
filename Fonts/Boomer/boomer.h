2bd8283a175cd859c812d9d96645d5dfe4256ecd
sachinkumar911 <sachinkumarmehta91@gmail.com>
2023-09-30 19:09:27 +0530

    adding A in boomer and add individual function for capptial letter and small letter

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -9,26 +9,27 @@ public:
 	{
 		return (colnumber != 0) ? ((colnumber * 7) + colnumber + colnumber) : 0;
 	}
-	void A(int colnumber)
+	
+	void a(int colnumber)
 	{
-		int col = col_num(colnumber);
+		int col=col_num(colnumber);
 
-		letters[0][2 + col] = letters[0][3 + col] = letters[0][5] = '_';
-		letters[1][1 + col] = '/';
-		letters[1][3 + col] = '_';
-		letters[1][4 + col] = '`';
-		letters[1][6 + col] = '|';
-		letters[2][0 + col] = '|';
-		letters[2][2 + col] = '(';
-		letters[2][3 + col] = '_';
-		letters[2][4 + col] = '|';
-		letters[2][6 + col] = '|';
-		letters[3][1 + col] = '\\';
-		letters[3][2 + col] = '_';
-		letters[3][3 + col] = '_';
-		letters[3][4 + col] = ',';
-		letters[3][5 + col] = '_';
-		letters[3][6 + col] = '|';
-	}
+		letters[2][2+col]=letters[2][3+col]=letters[2][5+col]='_';
+		letters[3][1+col]='/';letters[3][3+col]='_';letters[3][4+col]='`';letters[3][6+col]='|';
+	letters[4][0+col]='|';letters[4][2+col]='(';letters[4][3+col]='_';letters[4][4+col]='|';letters[4][6+col]='|';
+	letters[5][1+col]='\\';letters[5][2+col]='_';letters[5][3+col]='_';letters[5][4+col]=',';letters[5][5+col]='_';letters[5][6+col]='|';
+}
+
+void A(int colnumber)
+{
+	
+	int col =col_num(colnumber);
+	letters[0][2+col]=letters[0][3+col]=letters[0][4+col]=letters[1][3+col]=letters[2][3+col]=letters[3][3+col]=letters[5][1+col]=letters[5][5+col]='_';
+	letters[1][1+col]=letters[2][0+col]=letters[2][2+col]=letters[5][6+col]='/';
+	letters[1][5+col]=letters[2][4+col]=letters[2][6+col]=letters[5][0+col]='\\';
+	letters[3][0+col]=letters[3][6+col]=letters[4][0+col]=letters[4][2+col]=letters[4][4+col]=letters[4][6+col]=letters[5][2+col]=letters[5][4+col]='|';
+	
+
+}
 };
 #endif
