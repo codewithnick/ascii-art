5b9e525afe5ad9e5f38e6e1fc3e49c3936238d76
Akash Kumar <akashkumar59999@gmail.com>
2023-10-01 23:07:31 +0530

    added the code for boomer font B

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -27,5 +27,13 @@ public:
 		character[3][0]=character[3][6]=character[4][0]=character[4][2]=character[4][4]=character[4][6]=character[5][2]=character[5][4]='|';
 		return character;
 	}
+	char** B(){
+        char** character = getCharGrid();
+        character[0][0]=character[0][1]=character[0][2]=character[0][3]=character[0][4]=character[0][5]=character[1][2]=character[1][3]=character[1][4]=character[2][3]=character[3][2]=character[3][3]=character[3][4]=character[5][1]=character[5][2]=character[5][3]=character[5][4]=character[4][3]='_';
+        character[1][0]=character[2][0]=character[3][0]=character[4][0]=character[2][2]=character[4][2]='|';
+        character[1][6]=character[3][6]=character[5][0]='\\';
+        character[2][4]=character[2][6]=character[4][6]=character[5][5]=character[4][4]='/';
+        return character;
+	}
 };
 #endif
