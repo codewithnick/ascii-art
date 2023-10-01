386c9ee66f5644e85fe386b060f8125ac1720218
PRIYANSHU210021 <priyanshu210021@gmail.com>
2023-10-01 19:01:46 +0530

    Priyanshu

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -27,5 +27,25 @@ public:
 		character[3][0]=character[3][6]=character[4][0]=character[4][2]=character[4][4]=character[4][6]=character[5][2]=character[5][4]='|';
 		return character;
 	}
+
+	char** T()
+	{	
+	
+		char** character = getCharGrid();
+		character[0][0]=character[1][2]=character[1][3]=character[1][4]=character[2][0]=character[2][1]=character[2][3]=character[3][0]=character[3][1]=character[3][3]=character[4][0]=character[4][1]=character[4][3]=' ';
+		character[0][1]=character[0][2]=character[0][3]=character[0][4]=character[0][5]=character[1][1]=character[1][5]='_';
+		character[1][0]=character[1][6]=character[2][2]=character[2][4]=character[3][2]=character[3][4]=character[4][2]=character[4][4]=character[5][2]=character[5][4]='|';
+		character[5][2]='\\';
+		character[5][3]='_';
+		character[5][4]='/';
+		
+	
+
+
+		
+
+		return character;
+	}
+
 };
 #endif
