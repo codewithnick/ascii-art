62b3286ce3efaf993fad4de6231d965d0376caf1
P00P.head <tullu20top@gmail.com>
2023-10-04 00:22:17 +0530

    added x

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -591,17 +591,25 @@ public:
 
     */
 
-    /*
+    
         char **x()
         {
             char **character = getCharGrid();
 
             //Enter the character grid in ROWS X COLS
+            character[0][0]=character[0][1]=character[0][2]=character[0][3]=character[0][4]=character[0][5]=character[0][6]=' ';
+            character[1][0]=character[1][1]=character[1][2]=character[1][3]=character[1][4]=character[1][5]=character[1][6]=' ';
+            character[2][0]=character[2][1]=character[2][4]=character[2][5]=character[5][1]=character[5][4]='_';
+            character[3][1]=character[3][4]=character[4][0]=character[4][2]=character[4][3]=character[4][5]=' ';
+            character[3][0]=character[3][2]=character[5][3]=character[5][5]='\\';
+            character[3][3]=character[3][5]=character[5][0]=character[5][2]='/';
+            character[4][1]='>';
+            character[4][4]='<';
 
             return character;
         }
 
-    */
+    
 
     /*
         char **y()
