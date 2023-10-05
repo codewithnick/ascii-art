8813585412de01dc81cdede3287eb64c39c5cb80
Pranjal <pranjalkhatri950@gmail.com>
2023-10-05 23:34:33 +0530

    some more characters and bug fixes

--- Fonts/starwar/starwar.h
+++ Fonts/starwar/starwar.h
@@ -38,16 +38,24 @@ public:
     }
 
 
-    /*
-        char **C()
-        {
-            char **character = getCharGrid();
-
+    char **C()
+    {
+        char **character = getCharGrid();
+        
             //Enter the character grid in ROWS X COLS
+        character[1][0] = character[5][0] = character[0][0] = character[0][1] = ' ';
+        character[0][2] = character[0][3] = character[0][4] = character[0][5] = character[0][6] = character[0][7] = character[5][2] = character[5][3] = character[5][4] = character[5][5] = character[5][6] = character[5][7] = '_';
+        character[1][8] = character[2][0] = character[3][0] = character[4][0] = character[5][8] = character[3][3] = '|';
+        character[1][1] = '/';
+        character[5][1] = '\\';
+        character[2][3] = ',';
+        character[4][4] = character[4][5] = character[4][6] = character[4][7] = character[2][4] = character[2][5] = character[2][6] = character[2][7] = '-';
+        character[4][8] = '.';
+        character[4][3] = '`';
+        character[2][8] = '\'';
 
             return character;
-        }
-    */
+    }
 
     /*
         char **D()
@@ -93,16 +101,19 @@ public:
         }
     */
 
-    /*
+    
         char **H()
         {
             char **character = getCharGrid();
 
             //Enter the character grid in ROWS X COLS
+             character[1][0] = character[1][3] = character[1][5] = character[1][8] = character[2][0] = character[2][3] = character[2][5]= character[2][8]= character[3][0] = character[3][8] = character[4][0] =  character[4][3] = character[4][5] = character[4][8] = character[5][0] = character[5][3] = character[5][5] = character[5][8] ='|';
 
+            character[0][1] = character[0][2] = character[0][6] = character[0][7]= character[2][4]= character[3][4] = character[5][1] = character[5][2] = character[5][6] = character[5][7] = '_';
+            character[0][0] = character[0][4] = character[0][8] = character[1][1] = character[1][2] = character[1][6] = character[1][7] = character[2][1] = character[2][2] = character[2][6] = character[2][7] = character[3][1] = character[3][2] = character[3][6] = character[3][7] = character[4][1] = character[4][1] = character[4][1] = character[4][1] = character[4][1] = character[4][1] = character[4][2] = character[4][6] = character[4][7] = character[1][4] = character[4][4] = character[5][4] =  ' ';     
             return character;
         }
-    */
+    
 
     /*
         char **I()
