727e86b2eceaf97ebf6c0dd0cc7c8efc0bcaf69d
Atul-Xalxo <atulxalxo619@gmail.com>
2023-10-10 00:53:27 +0530

    Added boomer letter h.

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -416,17 +416,16 @@ public:
         character[2][2] = '(';
         return character;
     }
+     char** h()
+	{
 
-    /*
-        char **h()
-        {
-            char **character = getCharGrid();
+		char** character = getCharGrid();
+		character[0][1]=character[5][1]=character[5][5]=character[2][3]=character[2][4]=character[3][3]='_';
+		character[1][0]=character[2][0]=character[3][0]=character[4][0]=character[5][0]=character[1][2]=character[2][2]=character[4][2]=character[5][2]=character[4][4]=character[5][4]=character[4][6]=character[5][6]='|';
+		character[3][5]='\\';
+		return character ;
+	}
 
-            //Enter the character grid in ROWS X COLS
-
-            return character;
-        }
-    */
 
     char** i()
 	{
