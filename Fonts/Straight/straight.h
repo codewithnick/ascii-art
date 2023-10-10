c41a1023858fe40885ebd9ab92d11afd17498888
miragearush <arush.kumar@outlook.in>
2023-10-11 00:01:30 +0530

    Added E in straight

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -61,16 +61,17 @@ public:
         }
     */
 
-    /*
-        char **E()
-        {
+    char **E()
+    {
             char **character = getCharGrid();
 
             //Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][2] = '_';
+            character[1][0] = character[2][0] = '|';
+            character[1][1] = character[2][1] = character[2][2] = '_';
 
             return character;
         }
-    */
 
     /*
         char **F()
