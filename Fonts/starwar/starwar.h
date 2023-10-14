eadb9ffc9b8c0ed607c1d9cbba26ebf519ea713d
Neeraj.Sh-16 <shneeraj.2619@gmail.com>
2023-10-14 20:39:36 +0530

    Letter D added to starwar

--- Fonts/starwar/starwar.h
+++ Fonts/starwar/starwar.h
@@ -241,8 +241,8 @@ public:
 
             return character;
         }
+    
     */
-
     
         char **R()
         {
--- example.cpp
+++ example.cpp
@@ -5,12 +5,7 @@ using namespace ascii;
 
 int main()
 {
-<<<<<<< HEAD
     Ascii a = Ascii(starwar);
     a.print("D");
-=======
-    Ascii a = Ascii(straight);
-    a.print("p");
->>>>>>> f71ae0aad9aba0868ed82e85e46f0f6f37b9bbf3
     return 0;
 }
