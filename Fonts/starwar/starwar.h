c323a228621f5e6ee51e3035946dbca9ea3fd20b
chengm405 <146371159+chengm405@users.noreply.github.com>
2023-10-14 11:43:19 -0700

    Added character 8 to starwars

--- Fonts/starwar/starwar.h
+++ Fonts/starwar/starwar.h
@@ -781,17 +781,22 @@ public:
 
     */
 
-    /*
+    
         char **eight()
         {
             char **character = getCharGrid();
-
             //Enter the character grid in ROWS X COLS
-
+            character[0][2] = character[0][3] = character[0][4] = character[1][3] = character[2][3] = character[3][3] = character[4][3] = character[5][2] = character[5][3] = character[5][4] = '_';
+            character[1][1] = character[5][5] = '/';
+            character[5][1] = character[1][5] = '\\';
+            character[2][2] = character[4][2] = '(';
+            character[2][4] = character[4][4] = ')';
+            character[3][1] = '>';
+            character[3][5] = '<';
+            character[2][0] = character[2][6] = character[4][0] = character[4][6] = '|';
             return character;
         }
 
-    */
 
     /*
         char **nine()
