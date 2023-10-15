fc3aa67f5c4892f19cf7498511c81bf4d790b6f5
Neeraj.Sh-16 <shneeraj.2619@gmail.com>
2023-10-15 16:52:36 +0530

    Letter Y added to starwar

--- Fonts/starwar/starwar.h
+++ Fonts/starwar/starwar.h
@@ -328,16 +328,21 @@ public:
         return character;
     }
 
-    /*
+    
         char **Y()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid();
 
             //Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][2] = character[0][3] = character[0][0] = character[0][8] = character[0][9] = character[0][10] = character[0][11] = character[3][3] = character[3][8] = character[5][5] = character[5][6] = '_';
+            character[1][0] = character[2][1] = character[3][2] = character[1][4] = character[2][5] = '\\';
+            character[1][7] = character[2][6] = character[1][11] = character[2][10] = character[3][9] = '/';
+            character[4][4] = character[5][4] = character[4][7] = character[5][7] = '|';
+
 
             return character;
         }
-    */
+    
 
     /*
         char **Z()
--- example.cpp
+++ example.cpp
@@ -5,7 +5,7 @@ using namespace ascii;
 
 int main()
 {
-    Ascii a = Ascii(boomer);
-    a.print("AMWagjN");
+    Ascii a = Ascii(starwar);
+    a.print("Y");
     return 0;
 }
