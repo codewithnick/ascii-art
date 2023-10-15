0014a5d172d4c7d619ee0708fe9cfd5c91e06612
chengm405 <146371159+chengm405@users.noreply.github.com>
2023-10-15 10:20:07 -0700

    Added 1 to carlos

--- Fonts/carlos/carlos.h
+++ Fonts/carlos/carlos.h
@@ -897,17 +897,20 @@ public:
         character[1][6] = character[6][1] = '\\';
         return character;
     }
-    /*
+    
         char **one()
         {
             char **character = getCharGrid(ROWS,COLS);
 
-            //Enter the character grid in ROWS X COL
-
+            character[0][3] = character[0][4] = character[0][5] = character[0][6] = character[3][1] = character[6][4] = character[6][5] = '_';
+            character[1][2] = character[2][1] = character[3][0] = character[1][6] = character[2][3] = character[3][2] = character[6][3] = '/';
+            character[6][6] = '\\';
+            character[2][4] = character[4][4] = character[2][6] = character[4][6] = ')';
+            character[3][3] = character[5][3] = character[3][5] = character[5][5] = '(';
             return character;
         }
 
-    */
+    
 
     /*
         char **two()
