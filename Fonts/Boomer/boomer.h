9d605e1fc3a8950fa4e4614f8da0d8b197631983
vishal mishra <vishal.mishra9470@gmail.com>
2023-10-15 17:33:17 +0530

    Added number 8 to boomer

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -767,17 +767,20 @@ public:
 
     
 
-    /*
         char **eight()
         {
             char **character = getCharGrid();
 
-            //Enter the character grid in ROWS X COLS
+            character[0][1]  = character[0][2] = character[0][3] = character[0][4] =  character[0][5] = character[1][3] = character[3][3] = character[4][3] = character[5][1] = character[5][2] = character[5][3] = character[5][4] = character[5][5]  = '_'; 
+            character[1][0] = character[1][6] = character[4][0] = character[4][2] = character[4][4] = character[4][6] = '|';
+            character[2][1] = character[3][5] = character[5][0] = '\\';
+            character[2][5] = character[3][1] = character[5][6]  = '/';
+            character[2][3] = 'V';
+
 
             return character;
         }
 
-    */
 
     
         char **nine()
