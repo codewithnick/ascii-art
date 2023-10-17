b32e6b73bc62fc9e4e465f532a73b70f4f03bbad
s-xiae <146371043+s-xiae@users.noreply.github.com>
2023-10-17 10:08:51 -0700

    added the values for getCharGrid

--- Fonts/starwar/starwar.h
+++ Fonts/starwar/starwar.h
@@ -94,7 +94,7 @@ public:
 
     char **G()
     {
-        char **character = getCharGrid(ROWS,COLS);
+        char **character = getCharGrid(6,10);
 
         character[0][2] = character[0][3] = character[0][4] = character[0][5] = character[0][6] = character[0][7] = character[0][8] = character[1][4] = character[1][5] = character[1][6] = character[1][7] = character[1][8] = character[2][6] = character[2][7] = character[3][6] = character[4][4] = character[4][5] = character[5][2] = character[5][3] = character[5][4] = character[5][5] = character[5][6] = character[5][7] = '_';
         character[1][1] = '/';
