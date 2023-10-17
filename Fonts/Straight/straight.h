d259d41e7311e5075d01ae41d547adb5f54aeb5f
shivansh mishra <mishrashiv20003@gmail.com>
2023-10-18 00:16:34 +0530

    added 9 to straight

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -720,16 +720,19 @@ public:
 
     */
 
-    /*
+    
         char **nine()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(3,4);
 
-            //Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][2] = character[1][1] = character[1][2] = character[2][1] = character[2][2] = '_';
+            character[1][0] = '(';
+            character[2][3] = '/';
+            character[1][3] = '\\';
 
             return character;
         }
 
-    */
+    
 };
 #endif
