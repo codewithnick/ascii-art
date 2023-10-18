66d7c7b145cd77f19a54097ede7fb63f0c246727
Roshan  Poojari <127297054+Stunner33@users.noreply.github.com>
2023-10-18 23:58:57 +0530

    Added rows and columns

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -639,7 +639,7 @@ public:
     
         char **two()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(3,4);
 
             //Enter the character grid in ROWS X COLS
             character[0][1] = character[0][2]= character[2][2]= character[2][3]= character[1][2] ='_';
