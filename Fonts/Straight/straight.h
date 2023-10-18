03003e0e71b3b975a8a99070aa6d797e2e452118
shivansh mishra <mishrashiv20003@gmail.com>
2023-10-18 16:15:00 +0530

    added 4 in straight

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -660,17 +660,19 @@ public:
 
     */
 
-    /*
+    
         char **four()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(2,4);
 
-            //Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][2] = '_';
+            character[0][0] = character[0][3] = character[1][3] = '|';
+            
 
             return character;
         }
 
-    */
+    
 
     /*
         char **five()
@@ -708,19 +710,17 @@ public:
 
     */
 
-    
+    /*
         char **eight()
         {
-            char **character = getCharGrid(3,4);
+            char **character = getCharGrid(ROWS,COLS);
 
-            character[0][1] = character[0][2] = character[1][1] = character[1][2] = character[2][1] = character[2][2] = '_';
-            character[1][0] = character[2][0] = '(';
-            character[1][3] = character[2][3] = ')';
+            //Enter the character grid in ROWS X COLS
 
             return character;
         }
 
-
+    */
 
     
         char **nine()
