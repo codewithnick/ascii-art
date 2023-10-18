b40dc57ce4fcc12651c35165d47d0888d2245f2d
shivansh mishra <mishrashiv20003@gmail.com>
2023-10-18 15:45:51 +0530

    added six in straight

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -684,17 +684,20 @@ public:
 
     */
 
-    /*
+   
         char **six()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(3,4);
 
-            //Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][2] = character[1][1] = character[1][2] = character[2][1] = character[2][2] = '_';
+            character[1][0] = '/';
+            character[2][0] = '\\';
+            character[2][3] = ')';
 
             return character;
         }
 
-    */
+   
 
     /*
         char **seven()
