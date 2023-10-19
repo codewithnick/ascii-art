b32f75e1578c256041b9613ce901f58ef0f60077
Roshan  Poojari <127297054+Stunner33@users.noreply.github.com>
2023-10-20 03:00:40 +0530

    Added no '3' in banner.h

--- Fonts/banner/banner.h
+++ Fonts/banner/banner.h
@@ -622,8 +622,8 @@ public:
 
             //Enter the character grid in ROWS X COLS
             character[0][1]=character[0][2]=character[0][3]=character[0][4]=character[0][5]=character[1][0]=character[1][6]='#';
-	        character[2][6]=character[3][1]=character[3][2]=character[3][3]=character[3][4]=character[3][5]=character[4][6]='#';
-	        character[5][0]=character[6][1]=character[5][6]=character[6][2]=character[6][3]=character[6][4]=character[6][5]='#';
+	    character[2][6]=character[3][1]=character[3][2]=character[3][3]=character[3][4]=character[3][5]=character[4][6]='#';
+	    character[5][0]=character[6][1]=character[5][6]=character[6][2]=character[6][3]=character[6][4]=character[6][5]='#';
 
 
             return character;
