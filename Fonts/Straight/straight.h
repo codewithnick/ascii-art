ac5e1dc3616d92a6fef811fc61fa3d88f8b0d7d8
Vishal Patel <imvishal924@gmail.com>
2023-10-20 10:53:29 +0530

    added 5 in straight

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -620,17 +620,18 @@ public:
 
     // Numbers
 
-    /*
-        char **zero()
-        {
-            char **character = getCharGrid(ROWS,COLS);
+
+
+    /*    char **zero()
+    {
+       char **character = getCharGrid(ROWS,COLS);
 
             //Enter the character grid in ROWS X COLS
 
             return character;
-        }
-
+    }
     */
+    
 
     char **one()
     {
@@ -664,6 +665,7 @@ public:
 
             return character;
         }
+        
 
     */
 
@@ -677,17 +679,19 @@ public:
         return character;
     }
 
-    /*
+ 
+
         char **five()
-        {
-            char **character = getCharGrid(ROWS,COLS);
+    {
+        char **character = getCharGrid(3, 4);
+        character[0][0] = character[0][3] = character[1][3] = character[2][0] = ' ';
+        character[0][1] = character[0][2] = character[1][1] = character[1][2] = character[2][1] = character[2][2]= '_';
+        character[1][0] = character[2][3] = '|';
 
-            //Enter the character grid in ROWS X COLS
+        return character;
+    }
 
-            return character;
-        }
 
-    */
 
     char **six()
     {
