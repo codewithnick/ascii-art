0e72fa943cd06c9fd2cc25c8cb643866eefbe822
Anshul Namdev <anshulnamdev28@gmail.com>
2023-10-20 22:25:01 +0530

    Requested changes done

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -53,7 +53,7 @@ public:
     
         char **D()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(3,4);
 
             character[0][0] = character[0][3] = character[1][1] = character[1][2] = ' ';
             character[0][1] = character[0][2] = character[2][1] = character[2][2] = '_';
