8cbee1dec06b92d723a0eb3024efe4e8c5d330ab
Ahmad <heyahmad611@gmail.com>
2023-10-21 23:40:04 +0530

    Code added for letter P

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -204,16 +204,14 @@ public:
         return character;
     }
 
-    /*
-        char **P()
-        {
-            char **character = getCharGrid(ROWS,COLS);
-
-            //Enter the character grid in ROWS X COLS
-
-            return character;
-        }
-    */
+    char **P()
+    {
+        char **character = getCharGrid(3, 4);
+        character[0][1] = character[0][2] = character[1][1] = character[1][2] = '_';
+        character[1][0] = character[2][0] = '|';
+        character[1][3] = ')';
+        return character;
+    }
 
     /*
         char **Q()
--- example.cpp
+++ example.cpp
@@ -5,7 +5,7 @@ using namespace ascii;
 
 int main()
 {
-    Ascii a = Ascii(block);
-    a.print("N");
+    Ascii a = Ascii(straight);
+    a.print("NAP");
     return 0;
 }
