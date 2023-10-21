34e8786ba63315a8ddbb7e9e99269b0a94437d64
sandeep131431 <skumar131431@gmail.com>
2023-10-21 17:52:50 +0530

    add

--- Fonts/banner/banner.h
+++ Fonts/banner/banner.h
@@ -400,17 +400,18 @@ public:
 
     
 
-    /*
-        char **k()
+    char **k()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(7,5);
 
-            //Enter the character grid in ROWS X COLS
+            
+            character[0][0]=character[1][0]=character[2][0]=character[3][0]=character[4][0]=character[5][0]=character[2][2]=character[3][1]=character[4][2]=character[5][3]=character[6][0] = character[6][4]=character[1][3]='#';
+            character[0][1]=character[0][2]=character[0][3]=character[0][4]=character[1][1]=character[1][2]=character[1][3]=character[1][4]=character[2][1]=character[2][3]=character[2][4]=character[3][2]=character[3][3]=character[3][4]=character[4][1]=character[4][3]=character[4][4] =character[5][1]=character[5][2]=character[5][4]=character[6][1]=character[6][2]=character[6][3]= ' ';
 
             return character;
         }
 
-    */
+  
 
     /*
         char **l()
