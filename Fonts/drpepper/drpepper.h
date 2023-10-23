300a0e7268b8e6f6eb9779b9d3e624d149b7e76e
nk12nidhi12 <nk.nidhi.7632@gmail.com>
2023-10-23 23:19:08 +0530

    X added

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -265,16 +265,20 @@ public:
         }
     */
 
-    /*
+    
         char **X()
         {
-            char **character = getCharGrid(rows,cols);
+           
+             char **character = getCharGrid(4,5);
 
             //Enter the character grid in ROWS X COLS
+            character[0][1]=character[3][0]=character[0][4]=character[3][3]='_';
+            character[1][0]=character[1][2]=character[2][1]=character[2][3]=character[3][2]=character[3][4]='\\';
+            character[1][3]=character[3][1]='/';
 
             return character;
         }
-    */
+    
 
     /*
         char **Y()
