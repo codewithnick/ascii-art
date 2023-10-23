45ae063544b2bea4ca7a0b2ffc99b7d14444dfd8
mohdadil1 <adilm925@gmail.com>
2023-10-23 20:37:18 +0530

    Character y added to boomer

--- Fonts/Boomer/boomer.h
+++ Fonts/Boomer/boomer.h
@@ -645,17 +645,21 @@ public:
         return character;
     }
 
-    /*
+    
         char **y()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(6,7);
 
-            //Enter the character grid in ROWS X COLS
+        character[1][0]=character[1][2]=character[1][4]=character[1][6]=character[2][0]=character[2][2]=character[2][4]=character[2][6]=character[3][6]=character[4][6]=character[5][1]='|';
+        character[3][1]='\\';
+        character[0][1]=character[0][5]=character[2][3]=character[3][2]=character[3][3]=character[4][3]=character[4][2]=character[5][4]=character[5][3]=character[5][2]='_';
+        character[4][4]=character[5][5]='/';
+        character[3][4]=',';
 
             return character;
         }
 
-    */
+    
 
     char **z()
     {
