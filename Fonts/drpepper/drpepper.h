6df0ed1f28e5001d02b91edabc49ba3166822f44
sakshidewangan <148685066+sakshidewangan@users.noreply.github.com>
2023-10-24 13:51:16 +0530

    added I to drpepper

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -100,16 +100,17 @@ character[0][1]=character[0][3]=character[3][1]=character[3][3]='_';
         }
 
 
-    /*
-        char **I()
+   
+          char **I()
         {
-            char **character = getCharGrid(rows,cols);
+            char **character = getCharGrid(6,5);
 
-            //Enter the character grid in ROWS X COLS
+            character[1][0]=character[1][2]=character[2][0]=character[2][2]=character[3][0]=character[3][2]='|';
+character[0][1]=character[3][1]='_';
 
             return character;
         }
-    */
+
 
     /*
         char **J()
