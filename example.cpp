69de33983d0a0f70ed2e7b4a0d80c34125667964
mohdadil1 <adilm925@gmail.com>
2023-10-24 16:41:57 +0530

    Add character 2 to banner

--- example.cpp
+++ example.cpp
@@ -1,11 +1,11 @@
 #include "Ascii.h"
 #include "string"
-
+#include<iostream>
 using namespace ascii;
 
 int main()
 {
-    Ascii a = Ascii(straight);
-    a.print("7");
+    Ascii a = Ascii(banner);
+    a.print("2");
     return 0;
 }
