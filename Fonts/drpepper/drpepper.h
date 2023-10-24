b3de7297cc3183be1bc971a9e2de876f28fb10b8
sandeep131431 <skumar131431@gmail.com>
2023-10-24 20:14:23 +0530

    add

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -536,17 +536,18 @@ character[3][4] = '/';
 
     */
 
-    /*
+    
         char **t()
         {
-            char **character = getCharGrid(rows,cols);
-
-            //Enter the character grid in ROWS X COLS
+            char **character = getCharGrid(4,5);
+        character[0][2]=character[1][0]=character[1][4]=character[3][2] = '_';
+        character[1][1]=character[1][3]=character[2][1]=character[2][3]=character[3][1]=character[3][3]='|';
+            
 
             return character;
         }
 
-    */
+
 
     /*
         char **u()
@@ -673,17 +674,18 @@ character[3][4] = '/';
 
     */
 
-    /*
+
         char **four()
         {
-            char **character = getCharGrid(rows,cols);
-
-            //Enter the character grid in ROWS X COLS
+            char **character = getCharGrid(4,5);
+        character[0][2]=character[0][3]=character[2][2]=character[3][3]='_';
+        character[1][1]=character[2][0]='/';
+            
 
             return character;
         }
 
-    */
+
 
     /*
         char **five()
