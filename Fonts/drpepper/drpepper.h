dd8ced31695a704303e6c75adb61f5863699c607
sakshidewangan <148685066+sakshidewangan@users.noreply.github.com>
2023-10-24 13:37:43 +0530

    added H to drpepper

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -89,16 +89,16 @@ public:
         }
     */
 
-    /*
-        char **H()
+   char **H()
         {
-            char **character = getCharGrid(rows,cols);
+            char **character = getCharGrid(4,5);
 
-            //Enter the character grid in ROWS X COLS
+            character[1][0]=character[1][2]=character[1][4]=character[2][0]=character[2][4]=character[3][0]=character[3][2]=character[3][4]='|';
+character[0][1]=character[0][3]=character[3][1]=character[3][3]='_';
 
             return character;
         }
-    */
+
 
     /*
         char **I()
