f45dd5aac3560847b8c10e1c1cdf62c980ca7689
Abhishek Kumar <146465745+42Abhishek@users.noreply.github.com>
2023-10-24 22:08:37 +0530

    Update banner.h added s

--- Fonts/banner/banner.h
+++ Fonts/banner/banner.h
@@ -449,17 +449,15 @@ public:
         }
 
 
-    /*
-        char **s()
+    char **s()        
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(4,4);
 
-            //Enter the character grid in ROWS X COLS
-
+            // Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][2] = character[0][3] = character[1][0] = character[1][1] = character[2][2] = character[2][3] = character[3][0] =character[3][1] = character[3][2]  = '#';
+            character[0][0]=character[1][2]=character[1][3]=character[2][0]=character[2][1]=character[3][3]=' ';    
             return character;
         }
-
-    */
 
     /*
         char **t()
