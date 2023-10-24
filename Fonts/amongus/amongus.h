866e03982deed3f46ca7dc9b5064baf2c377a246
Sunil Kumar <sunilbehera672@gmail.com>
2023-10-24 17:26:17 +0530

    added F to amongus fonts

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -79,16 +79,20 @@ public:
         }
     
 
-    /*
+    
         char **F()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(4,6);
+            character[0][1]= character[0][2]= character[0][3]= character[0][4]= character[1][2]= character[1][3]= character[1][4]= character[2][2]= character[2][3]= character[3][1]= character[3][2]='_';
+            character[1][0]= character[3][0]='(';
+            character[1][5]= character[2][1]= character[2][4]= character[3][3]=')';
 
-            //Enter the character grid in ROWS X COLS
+
+            
 
             return character;
         }
-    */
+    
 
     /*
         char **G()
