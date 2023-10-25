21985a896880b551d52d0f11ae76b23b855ae829
Sunil Kumar <sunilbehera672@gmail.com>
2023-10-25 17:34:11 +0530

    added 2 to amongus font

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -685,17 +685,22 @@ public:
 
     */
 
-    /*
+    
         char **two()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(4,6);
+            character[0][1]=character[0][2]=character[0][3]=character[1][1]=character[1][2]=character[2][3]=character[3][1]=character[3][2]=character[3][3]=character[3][4]='_';
+            character[1][0]=character[3][0]='(';
+            character[2][1]=character[2][4]='/';
+            character[1][4]='\\';
+            character[3][5]=')';
 
-            //Enter the character grid in ROWS X COLS
+            
 
             return character;
         }
 
-    */
+    
 
     /*
         char **three()
