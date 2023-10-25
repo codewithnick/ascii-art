4bd16434471a2dc1a52dd7625d846753d1627113
Gourab Bistu <gourabbistu2002@gmail.com>
2023-10-25 18:23:20 +0530

    Adding Character I in font amongush

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -128,16 +128,17 @@ public:
             return character;
         }
 
-    /*
+    
         char **I()
         {
-            char **character = getCharGrid(ROWS,COLS);
-
-            //Enter the character grid in ROWS X COLS
+            char **character = getCharGrid(4,6);
+            character[0][1] = character[0][2] = character[0][3] = character[0][4] = character[1][1] = character[1][4] = character[2][1] = character[2][4] = character[3][1] = character[3][2] = character[3][3] = character[3][4] = '_';
+            character[1][0] = character[3][0] = character[2][3] = '(';
+            character[1][5] = character[3][5] = character[2][2] = ')';
 
             return character;
         }
-    */
+    
 
         char **J()
         {
--- example.cpp
+++ example.cpp
@@ -6,6 +6,6 @@ using namespace ascii;
 int main()
 {
     Ascii a = Ascii(amongus);
-    a.print("H");
+    a.print("I");
     return 0;
 }
