b2678b36cb6451d598edb196483704e7ef33e9d9
ascii art <sk5588785@gmail.com>
2023-10-27 21:56:47 +0530

    adding character n in amongus font...

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -519,6 +519,19 @@ public:
         }
 
     */
+   char **n()
+    {
+        char **character = getCharGrid(4, 6);
+
+        // Enter the character grid in ROWS X COLS
+        character[0][1] = character[0][4] = character[3][1] = character[3][4] = '_';
+        character[1][0] = character[1][3] = character[3][0] = character[2][4] ='(';
+        character[1][5] = character[2][1] = character[3][2] = character[3][5] = ')';
+        character[1][2] = character[3][3] = '\\';
+       // character[0][0] = character[0][2] = character[0][3] = character[0][5] = character[1][1] = character[1][4] = character[2][0] =character[2][2] =character[2][3] = character[2][5]  = ' ';
+
+        return character;
+    }
 
     /*
         char **o()
