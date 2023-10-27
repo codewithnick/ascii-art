4f91569e59b7d4e45758e511be9454a2519d3623
Pratik Kashyap <135964490+ipratiik@users.noreply.github.com>
2023-10-28 00:10:15 +0530

    Added character <W> to font <amongus>

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -300,16 +300,18 @@ public:
         return character;
     }
 
-    /*
-        char **W()
-        {
-            char **character = getCharGrid(ROWS,COLS);
-
-            //Enter the character grid in ROWS X COLS
-
-            return character;
-        }
-    */
+    char **W()
+    {
+        char **character = getCharGrid(4, 8);
+        character[0][1] = character[0][6] = character[3][1] = character[3][2] = character[3][5] = character[3][6] = '_';
+        character[1][0] = character[3][0] = '(';
+        character[1][7] = character[3][7] = ')';
+        character[1][3] = character[1][5] = character[3][3] = '/';
+        character[1][2] = character[1][4] = character[3][4] = '\\';
+        character[2][1] = ')';
+        character[2][6] = '(';
+        return character;
+    }
 
     char **X()
     {
