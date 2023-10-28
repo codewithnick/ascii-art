81d0bebab010dbd080b2854f2c8a8b13e8ead997
Vishal Patel <imvishal924@gmail.com>
2023-10-28 14:51:31 +0530

    Added P in straight

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -196,16 +196,19 @@ public:
         return character;
     }
 
-    /*
+    
         char **P()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(3,4);
 
             //Enter the character grid in ROWS X COLS
+            character[0][0] =' ', character[0][1] = character[0][2]='_'  ;
+            character[1][0] ='|', character[1][1] = character[1][2]= '_' ,character[1][3] = ')';
+            character[2][0] ='|', character[2][1] = character[2][2]= ' ';
 
             return character;
         }
-    */
+    
 
     /*
         char **Q()
