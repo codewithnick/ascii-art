5e22b4056200e0b82d91a89dbf9294e70e302325
@ranveer-2301 <ranveerkrpoddar@gmail.com>
2023-10-28 21:46:36 +0530

    added k in amongus font

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -473,17 +473,17 @@ public:
         return character;
         }
 
-    /*
-        char **k()
+    char **k()
         {
-            char **character = getCharGrid(ROWS,COLS);
-
-            //Enter the character grid in ROWS X COLS
-
+            char **character = getCharGrid(4,6);
+            character[0][1] = character[0][4] = character[3][1] = character[3][4] = '_';
+            character[1][0] = character[2][4] = character[3][0] =  '(';
+            character[1][2] = character[1][5] = character[2][1] = character[3][2] = character[3][5] = ')';
+            character[1][3] = '/';
+            character[3][3] = '\\';
             return character;
         }
 
-    */
 
     /*
         char **l()
