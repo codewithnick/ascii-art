4b39306acb48dd9e839e7ba7ce8bccf35de26224
Pratik Kashyap <135964490+ipratiik@users.noreply.github.com>
2023-10-28 19:59:18 +0530

    Added digit <5> to font <amongus>
    I had replaced hypen with underscore as per requested.

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -782,7 +782,7 @@ character[0][2]=character[0][3]=character[2][4]=character[2][1]=character[3][3]=
     char **five()
         {
             char **character = getCharGrid(4,5);
-            character[0][1]=character[0][2]=character[0][3]=character[1][2]=character[1][3]=character[2][1]=character[2][2]=character[3][1]=character[3][2]=character[3][3]='-';
+            character[0][1]=character[0][2]=character[0][3]=character[1][2]=character[1][3]=character[2][1]=character[2][2]=character[3][1]=character[3][2]=character[3][3]='_';
             character[1][0]=character[2][0]='|';
             character[1][4]=')';
             character[3][0]='(';
