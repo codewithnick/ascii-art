0657772d0f72d9ac5ebf45081652c5ca2eecface
starngerteam <bhaskarsingh91642@gmail.in>
2023-10-28 16:40:43 +0530

    added s in font drpepper

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -260,16 +260,21 @@ character[3][3] = '/';
         return character;
     }
 
-    /*
-        char **W()
-        {
-            char **character = getCharGrid(rows,cols);
 
-            //Enter the character grid in ROWS X COLS
+         char **W()
+         {
+            char **character = getCharGrid(4, 7);
+ 
+             //Enter the character grid in ROWS X COLS
+            character[0][1] = character[0][3] = character[0][5] = character[3][1] = character[3][2] = character[3][4] = '_';
+            character[1][0] = character[1][2] = character[1][4] = character[1][6] = character[2][0] = character[2][2] = character[2][4] = character[2][6] = character[3][0] = '|';
+            character[3][3] = character[3][5] = '/';
+ 
+             return character;
+         }
 
-            return character;
-        }
-    */
+
+
 
     char **X()
     {
@@ -286,7 +291,7 @@ character[3][3] = '/';
 
      char **Y()
         {
-            char **character = getCharGrid(rows,cols);
+        
             char **character = getCharGrid(4, 5);
             //Enter the character grid in ROWS X COLS
             character[0][1] = character[0][3] = character[3][2] = '_';
@@ -538,12 +543,12 @@ character[2][0] = character[2][2] = character[3][0] = character[3][2] = '|';
 
         char **s()
     {
-        char **character = getCharGrid(4, 5);
-        character[0][0] = ' ';
-character[0][1] = character[0][2] = character[0][3] = character[1][1] = character[2][1] = character[2][2] = '_';
-character[1][0] = character[1][3] = '<';
-character[1][2] = '-';
-character[2][0] = character[2][3] = '/';
+        char **character = getCharGrid(5,5);
+        character[0][0] = character[0][1] = character[0][2] = character[0][3] = character[1][0] = ' ';
+character[1][1] = character[1][2] = character[1][3] = character[2][1] = character[3][1] = character[3][2] = '_';
+character[2][0] = character[2][3] = '<';
+character[2][2] = '-';
+character[3][0] = character[3][3] = '/';
         return character;
     }
 
