87435c03950f15b4044670c3e3d2077c18a58faf
ascii art <sk5588785@gmail.com>
2023-10-29 22:48:32 +0530

    adding r character in amongus font . . .

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -639,15 +639,15 @@ public:
 
     */
 
-    char **z()
-    {
-        char **character = getCharGrid(ROWS, COLS);
-        character[0][1] = character[0][2] = character[0][3] = character[0][4] = character[1][1] = character[2][4] = character[3][1] = character[3][2] = character[3][3] = character[3][4] = '_';
-        character[1][0] = character[3][0] = '(';
-        character[2][1] = character[2][3] = '/';
-        character[1][5] = character[3][5] = ')';
-        return character;
-    }
+    // char **z()
+    // {
+    //     char **character = getCharGrid(ROWS, COLS);
+    //     character[0][1] = character[0][2] = character[0][3] = character[0][4] = character[1][1] = character[2][4] = character[3][1] = character[3][2] = character[3][3] = character[3][4] = '_';
+    //     character[1][0] = character[3][0] = '(';
+    //     character[2][1] = character[2][3] = '/';
+    //     character[1][5] = character[3][5] = ')';
+    //     return character;
+    // }
 
     // Numbers
 
@@ -723,19 +723,19 @@ public:
         return character;
     }
 
-    char **six()
-    {
-        char **character = getCharGrid(ROWS, COLS);
+    // char **six()
+    // {
+    //     char **character = getCharGrid(ROWS, COLS);
 
-        // Enter the character grid in ROWS X COLS
-        character[0][2] = character[2][2] = '_';
-        character[1][1] = character[2][0] = character[3][4] = '/';
-        character[3][0] = character[2][4] = '\\';
-        character[1][3] = ')';
-        character[3][1] = character[3][2] = character[3][3] = '_';
+    //     // Enter the character grid in ROWS X COLS
+    //     character[0][2] = character[2][2] = '_';
+    //     character[1][1] = character[2][0] = character[3][4] = '/';
+    //     character[3][0] = character[2][4] = '\\';
+    //     character[1][3] = ')';
+    //     character[3][1] = character[3][2] = character[3][3] = '_';
 
-        return character;
-    }
+    //     return character;
+    // }
 
     char **seven()
     {
