b7a10dd72a0ff089e2965981ac4558881ff5a87c
ascii art <sk5588785@gmail.com>
2023-10-29 09:33:48 +0530

    adding r character in amongus font . . .

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -613,7 +613,19 @@ public:
         }
 
     */
+     char **r()
+    {
+        char **character = getCharGrid(4, 6);
 
+        // Enter the character grid in ROWS X COLS
+        character[0][1] = character[0][2] = character[0][3] = character[0][4] = character[1][3] = character[1][3] = character[3][1] = character[3][4] = '_';
+        character[1][5] = character[3][3] =  '\\';
+        character[3][5] = character[2][1] = character[3][2] = ')';
+        character[1][0] = character[3][0] ='(';
+        character[2][5] ='/';
+
+        return character;
+    }
     /*
         char **s()
         {
