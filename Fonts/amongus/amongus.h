56c1f9319393b86ac2843096f6e9cf91160f858b
shivamgupta1990 <shivamgupta1700000@gmail.com>
2023-10-29 23:07:19 +0530

    added f in amongus font

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -404,17 +404,14 @@ public:
             character[2][1]=character[1][5]=character[2][4]=character[3][5]=')';
             return character;
         }
-    /*
-        char **f()
+     char **f()
         {
             char **character = getCharGrid(ROWS,COLS);
-
-            //Enter the character grid in ROWS X COLS
-
+            character[0][1]= character[0][2]= character[0][3]= character[0][4]= character[1][2]= character[1][3]= character[1][4]= character[2][2]= character[2][3]= character[3][1]= character[3][2]= '_';
+            character[1][5]= character[2][1]= character[2][4]= character[3][3]=')';
+            character[1][0]= character[3][0]='(';
             return character;
         }
-    */
-
     /*
         char **g()
         {
