91e6f6b2c8a441a65bf0fef5c0dee83459a9d815
Divyanshu <bautocrats@gmail.com>
2023-10-30 15:42:38 +0530

    fixed char 4 in drpepper

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -627,19 +627,14 @@ public:
 
     char **four()
     {
-        char **character = getCharGrid(5, 4);
-        for (int i = 0; i < 4; i++)
-        {
-            character[0][i] = ' ';
-            character[1][i] = '_';
-            character[2][i] = ' ';
-        }
-        character[3][0] = '/';
-        character[3][3] = '|';
-        character[4][0] = '/';
-        character[4][1] = '|';
-        character[4][2] = '|';
-        character[4][3] = '\\';
+        char **character = getCharGrid(5, 6);
+
+        character[0][0] = character[0][1] = character[0][4] = character[0][5] = character[1][0] = character[1][3] = character[1][5] = character[2][2] = character[2][3] = character[3][0] = character[3][1] = character[3][5] = ' ';
+        character[0][2] = character[0][3] = character[3][3] = character[2][1] = character[3][3] = '_';
+        character[1][1] = character[2][0] = '/';
+        character[1][2] = character[2][4] = '.';
+        character[1][4] = character[2][5] = character[3][2] = character[3][4] = '|';
+
         return character;
     }
 
