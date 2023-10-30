dacd89656d53d5ee640029188a86cf60ca4eea4e
index.html <spronajit@gmail.com>
2023-10-30 06:41:23 +0530

    added 9 for font amongus

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -396,7 +396,7 @@ public:
         return character;
     }
 
-    char **e(
+    char **e()
         {
             char **character = getCharGrid(4,6);
             character[0][1]=character[0][2]=character[0][3]=character[0][4]=character[1][2]=character[1][3]=character[1][4]=character[2][2]=character[2][3]=character[3][1]=character[3][2]=character[3][3]=character[3][4]='_';
@@ -759,16 +759,19 @@ public:
         return character;
     }
 
-    /*
+
         char **nine()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(4,5);
+            character[0][1] = character[0][2] = character[0][3] =  character[1][2] =  character[2][1] =  character[3][2] = '_';
+            character[1][0] = character[2][4] =  character[3][3] = '/';
+             character[1][4] =  character[2][0] = '\\';
+              character[3][1] = '(';
 
-            //Enter the character grid in ROWS X COLS
-
+           
             return character;
         }
 
-    */
+
 };
 #endif
