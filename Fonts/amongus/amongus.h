d3732e1b11fddd48bb789129a63131dbe5f02d6b
Leena Mondal <146467319+LeenaMondal01@users.noreply.github.com>
2023-10-31 18:36:54 +0530

    Added the letter 'w' in amongus.h
    Added the letter 'w' in amongus.h

--- Fonts/amongus/amongus.h
+++ Fonts/amongus/amongus.h
@@ -599,18 +599,17 @@ public:
         }
 
 
-    /*
-        char **w()
+  char **w()
         {
-            char **character = getCharGrid(ROWS,COLS);
+            char **character = getCharGrid(4,8);
 
-            //Enter the character grid in ROWS X COLS
-
+            character[0][1] = character[0][6] = character[3][1] = character[3][2] = character[3][5] = character[3][6] = '_';
+            character[1][0] = character[2][6] = character[3][0] = '(';
+            character[1][7] = character[2][1] = character[3][7] = ')';
+            character[1][2] = character[1][4] = character[3][4] = '\\';
+            character[1][3] = character[1][5] = character[3][3] = '/';
             return character;
         }
-
-    */
-
     char **x()
     {
         char **character = getCharGrid(4, 6);
