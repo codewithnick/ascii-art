61a89755b682d69a62e97df9b0a76f9b6401aeec
Rishav <rishavr920@gmail.com>
2023-10-31 19:16:25 +0530

    added m in drpepper

--- Fonts/drpepper/drpepper.h
+++ Fonts/drpepper/drpepper.h
@@ -453,9 +453,10 @@ public:
 
             //Enter the character grid in ROWS X COLS
             character[0][0]='.';
-            character[0][1]=character[0][3]=character[0][5]=character[2][1]=character[2][3]=character[2][5]='_';
-            character[1][0]=character[2][0]=character[1][6]=character[2][6]=character[2][2]=character[2][4]='|';
+            character[0][1]=character[0][3]=character[0][5]=character[3][1]=character[3][3]=character[3][5]='_';
+            character[1][0]=character[2][0]=character[1][6]=character[2][6]=character[3][2]=character[3][4]=character[3][0]=character[3][6]='|';
             character[1][2]=character[1][4]='\'';
+            
             return character;
         }
 
