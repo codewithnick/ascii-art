5eeb9be0eec7bfd29ba3aef6f96556851b8a1634
choidf <39011839+choidf@users.noreply.github.com>
2023-10-31 19:29:27 +0700

    fix bottom aligning

--- Fonts/Straight/straight.h
+++ Fonts/Straight/straight.h
@@ -487,11 +487,11 @@ public:
 
     char **o()
     {
-        char **character = getCharGrid(ROWS, COLS);
+        char **character = getCharGrid(3, 3);
 
-        character[0][1] = character[1][1] = '_';
-        character[1][0] = '(';
-        character[1][2] = ')';
+        character[1][1] = character[2][1] = '_';
+        character[2][0] = '(';
+        character[2][2] = ')';
 
         return character;
     }
