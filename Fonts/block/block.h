449c76508c428382adab3eff87e1769f675a1a6c
Sriparno Roy <89148144+Sriparno08@users.noreply.github.com>
2024-10-07 19:23:05 +0530

    Add character: <g> to font: <Block> (#1127)

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -623,16 +623,24 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
         }
     */
 
-    /*
-        vs g()
-        {
-            vs character = getCharGrid();
+    vs g()
+    {
+        vs character = getCharGrid(11, 20);
 
-            //Enter the character grid in ROWS X COLS
+        character[0] =  " .----------------. ";
+        character[1] =  "| .--------------. |";
+        character[2] =  "| |    ______    | |";
+        character[3] =  "| |  .' ___  |   | |";
+        character[4] =  "| | / .'   \\_|   | |";
+        character[5] =  "| | | |    ____  | |";
+        character[6] =  "| | \\ `.___]  _| | |";
+        character[7] =  "| |  `._____.'   | |";
+        character[8] =  "| |              | |";
+        character[9] =  "| '--------------' |";
+        character[10] = " '----------------' ";
 
-            return character;
-        }
-    */
+        return character;
+    }
 
     vs h()
     {
