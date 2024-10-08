ddc838a7cba40c1d6fafde66396e7377313329cf
nakul-py <nakulverma.py@gmail.com>
2024-10-08 14:43:38 +0530

    character <a> added to block

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -531,16 +531,26 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
 
     // Lowercase
 
-    /*
+    
         vs a()
         {
-            vs character = getCharGrid();
-
-            //Enter the character grid in ROWS X COLS
+            vs character = getCharGrid(11, 20);
+            
+            character[0] =  " .----------------. ";
+            character[1] =  "| .--------------. |";
+            character[2] =  "| |      __      | |";
+            character[3] =  "| |     /  \     | |";
+            character[4] =  "| |    / /\ \    | |";
+            character[5] =  "| |   / /__\ \   | |";
+            character[6] =  "| | _/ /    \ \_ | |";
+            character[7] =  "| |!____!  !____!| |";
+            character[8] =  "| |              | |";
+            character[9] =  "| '--------------' |";
+            character[10] = " '----------------' ";
 
             return character;
         }
-    */
+    
 
     
     vs b()
