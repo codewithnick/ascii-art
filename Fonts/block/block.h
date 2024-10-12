aef5bb85530ad3b9861f155f8f1a2d427b1494da
nakul-py <nakulverma.py@gmail.com>
2024-10-12 19:19:00 +0530

    feat/add-n-to-font-block

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -779,17 +779,28 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
 
     */
 
-    /*
+    
         vs n()
         {
-            vs character = getCharGrid();
+            vs character = getCharGrid(11, 20);
 
-            //Enter the character grid in ROWS X COLS
+           character[0] = " .----------------. ";
+           character[1] = "| .--------------. |";
+           character[2] = "| | ____  _____  | |";
+           character[3] = "| ||_   \\|_   _| | |";
+           character[4] = "| |  |   \\ | |   | |";
+           character[5] = "| |  | |\\ \\| |   | |";
+           character[6] = "| | _| |_\\   |_  | |";
+           character[7] = "| ||_____|\\____| | |";
+           character[8] = "| |              | |";
+           character[9] = "| '--------------' |";
+           character[10]= " '----------------' ";
+
 
             return character;
         }
 
-    */
+    
 
     /*
         vs o()
