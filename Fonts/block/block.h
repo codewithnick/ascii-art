663485c0ad70aa2f3f1c24ffa36933431cb36003
nakul-py <nakulverma.py@gmail.com>
2024-10-12 18:58:46 +0530

    feat/add-k-to-font-block

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -743,17 +743,27 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
         return character;
     }
 
-    /*
+    
         vs k()
         {
-            vs character = getCharGrid();
+            vs character = getCharGrid(11, 20);
 
-            //Enter the character grid in ROWS X COLS
+           character[0] = " .----------------. ";
+           character[1] = "| .--------------. |";
+           character[2] = "| |  ___  ____   | |";
+           character[3] = "| | |_  ||_  _|  | |";
+           character[4] = "| |   | |_/ /    | |";
+           character[5] = "| |   |  __'.    | |";
+           character[6] = "| |  _| |  \ \_  | |";
+           character[7] = "| | |____||____| | |";
+           character[8] = "| |              | |";
+           character[9] = "| '--------------' |";
+           character[10]= " '----------------' ";
 
             return character;
         }
 
-    */
+    
 
     /*
         vs l()
