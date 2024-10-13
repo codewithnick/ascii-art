b0b2aaef6080a566435a2073cb61377e5543d039
nakul-py <nakulverma.py@gmail.com>
2024-10-13 10:22:38 +0530

    feat/add-char-q-to-font-block

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -867,17 +867,27 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
 
     */
 
-    /*
+    
         vs q()
         {
-            vs character = getCharGrid();
+            vs character = getCharGrid(11 ,20);
 
-            //Enter the character grid in ROWS X COLS
+           character[0] = " .----------------. ";
+           character[1] = "| .--------------. |";
+           character[2] = "| |    ___       | |";
+           character[3] = "| |  .'   '.     | |";
+           character[4] = "| | /  .-.  \\    | |";
+           character[5] = "| | | |   | |    | |";
+           character[6] = "| | \\  `-'  \\_   | |";
+           character[7] = "| |  `.___.\\__|  | |";
+           character[8] = "| |              | |";
+           character[9] = "| '--------------' |";
+           character[10]= " '----------------' ";
 
             return character;
         }
 
-    */
+    
 
     /*
         vs r()
