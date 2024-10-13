0cdd5e76b0c7264d2d1ceec0b67bfda9ee72b4a1
nakul-py <nakulverma.py@gmail.com>
2024-10-13 10:42:34 +0530

    feat/add-char-s-to-font-block

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -891,17 +891,27 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
 
     */
 
-    /*
+    
         vs s()
         {
-            vs character = getCharGrid();
+            vs character = getCharGrid(11 ,20);
 
-            //Enter the character grid in ROWS X COLS
+            character[0] = " .----------------. ";
+            character[1] = "| .--------------. |";
+            character[2] = "| |    _______   | |";
+            character[3] = "| |   /  ___  |  | |";
+            character[4] = "| |  |  (__ \\_|  | |";
+            character[5] = "| |   '.___`-.   | |";
+            character[6] = "| |  |`\\____) |  | |";
+            character[7] = "| |  |_______.'  | |";
+            character[8] = "| |              | |";
+            character[9] = "| '--------------' |";
+            character[10]= " '----------------' ";
 
             return character;
         }
 
-    */
+    
 
     vs t()
     {
