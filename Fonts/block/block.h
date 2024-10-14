2f1f9d13e063f06a95e7df867509a857c239b33c
nakul-py <nakulverma.py@gmail.com>
2024-10-14 18:22:41 +0530

    [feature:- adding x to block (#1179)

--- Fonts/block/block.h
+++ Fonts/block/block.h
@@ -999,17 +999,27 @@ character[4][11]=character[4][13]=character[5][10]=character[5][12]='/';
 
     */
 
-    /*
+    
         vs x()
         {
-            vs character = getCharGrid();
+            vs character = getCharGrid(11, 20);
 
-            //Enter the character grid in ROWS X COLS
+           character[0] = " .----------------. ";
+           character[1] = "| .--------------. |";
+           character[2] = "| |  ____  ____  | |";
+           character[3] = "| | |_  _||_  _| | |";
+           character[4] = "| |   \\ \\  / /   | |";
+           character[5] = "| |    > `' <    | |";
+           character[6] = "| |  _/ /'`\\ \\_  | |"; 
+           character[7] = "| | |____||____| | |";
+           character[8] = "| |              | |";
+           character[9] = "| '--------------' |";
+           character[10]= " '----------------' ";
 
             return character;
         }
 
-    */
+    
 
     
         vs y()
