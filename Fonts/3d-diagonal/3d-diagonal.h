8df4a262c6e5d9daba71c37a281b40d48d0343f7
Avijit Dey <avijitdey2402@gmail.com>
2024-10-31 23:50:35 +0530

    add letter J to 3d-diagonal font

--- Fonts/3d-diagonal/3d-diagonal.h
+++ Fonts/3d-diagonal/3d-diagonal.h
@@ -134,16 +134,29 @@ public:
         }
     */
 
-    /*
+
         vs J()
         {
-            vs character = getCharGrid();
+            vs character = getCharGrid(14, 19);
 
-            //Enter the character grid in ROWS X COLS
+                character[0]  = "         ,---._    ";
+                character[1]  = "       .-- -.' \\   ";
+                character[2]  = "       |    |   :  ";
+                character[3]  = "       :    ;   |  ";
+                character[4]  = "       :        |  ";
+                character[5]  = "       |    :   :  ";
+                character[6]  = "       :           ";
+                character[7]  = "       |    ;   |  ";
+                character[8]  = "   ___ :           ";
+                character[9]  = " /    /\\    J   :  ";
+                character[10] = "/  ../  `..-    ,  ";
+                character[11] = "\\    \\         ;  ";
+                character[12] = " \\    \\      ,'   ";
+                character[13] = "  \"---....--'     ";
 
             return character;
         }
-    */
+
 
     /*
         vs K()
